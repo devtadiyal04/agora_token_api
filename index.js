90ce6c583889f808ad894a2a4bd55d0dc765a06b
DEVENDERA TADIYAL <32829271+devtadiyal04@users.noreply.github.com>
2025-06-27 06:16:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { RtmTokenBuilder, RtmRole } = require('agora-access-token');
+const { RtmTokenBuilder, RtmRole } = require('agora-token');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
